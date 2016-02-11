b8ad06e8a3bf6a62eb9440c231d76c561b392f0c
evanfeinberg <enf@vsp-compute-40.stanford.edu>
2016-02-11 01:03:35 -0800

    added ligand_protein featurization

---
+++ mor_ligand_atom_residue_feature_types.py
@@ -0,0 +1,39 @@
+from residue import Residue
+from tutorial_tica_config import inactive_dir, active_dir, base, simulation_structure
+from get_variable_names import get_common_residues_pkl, find_common_residues
+
+def convert_list_to_resobject_list(contact_residues):
+  resobject_list = []
+  for residue in contact_residues:
+    new_residue = Residue(resSeq = residue[1], chain_id = residue[0])
+    resobject_list.append(new_residue)
+  return(resobject_list)
+
+#CHOOSE RESIDUES:
+
+
+all_residues = list(range(29,340))
+
+tm6_tm3_residues = convert_list_to_resobject_list([("A",279), ("A",165)])
+print("tm6_tm3_residues")
+print(tm6_tm3_residues)
+npxxy_residues =  convert_list_to_resobject_list([("A", r) for r in range(332,337)])
+
+
+cutoff = 0.5
+#feature_name = "all_residues_4dkl_5c1m_under_cutoff%dA" %(int(10*cutoff))
+feature_name = "protein_ligand_atom_contacts"
+
+feature_name_residues_dict = {}
+feature_name_residues_dict["tm6_tm3_dist"] = tm6_tm3_residues
+feature_name_residues_dict["rmsd_npxxy_active"] = npxxy_residues
+feature_name_residues_dict["rmsd_npxxy_inactive"] = npxxy_residues
+
+common_residues_pkl = get_common_residues_pkl(base)
+contact_residues = find_common_residues([inactive_dir, active_dir, simulation_structure], common_residues_pkl)
+
+#ligand_residue = Residue(resSeq = 900, chain_id = "L", res_name = "LIG")
+
+#contact_residues += [ligand_residue]
+
+
---
+++ mor_ligand_atom_residue_tica_config.py
@@ -0,0 +1,61 @@
+import os
+import sys
+import csv
+
+exacycle = False
+
+
+n_clusters = 100
+lag_time = 5
+if exacycle:
+  lag_time *= 2
+
+msm_lag_time = 5
+n_components = 10
+n_samples = 10
+n_macrostates = 25
+n_trees = 100
+
+precision = "SP"
+
+sparse = True
+wolf = True
+
+is_sparse = ""
+if(sparse): is_sparse = "sparse-"
+
+shrinkage_string = ""
+rho_string = ""
+if wolf and not sparse:
+  wolf_string = "_wolf_"
+  shrinkage = None
+  shrinkage_string = "autoShrinkage"
+  rho = None
+  rho_string = ""
+elif wolf and sparse:
+  wolf_string = "_wolf_"
+  shrinkage = None
+  shrinkage_string = "autoShrinkage"
+  rho = 0.005
+  rho_string = "_rho0pt005"
+else:
+  wolf_string = ""
+  shrinkage = 0.001
+  shrinkage_string = "0pt001"
+  rho = None
+  rho_string = ""
+
+traj_ext = ".h5"
+base = "/home/enf/quintin/Post_Process/GPCR/MOR/LIG_path/BU_path"	
+traj_dir = "/home/enf/quintin/Post_Process/GPCR/MOR/LIG_path/BU_path/h5_trajectories"
+structure = None
+pnas_features_dir = "/home/enf/quintin/Post_Process/GPCR/MOR/LIG_path/BU_path/pnas_features"
+if not os.path.exists(pnas_features_dir): os.makedirs(pnas_features_dir)
+iterative = False
+featurize_parallel = True
+inactive_dir = "/home/enf/quintin/Post_Process/GPCR/MOR/4dkl_R_for_conformation.pdb"
+active_dir = "/home/enf/quintin/Post_Process/GPCR/MOR/5c1m.pdb"
+simulation_structure = "/home/enf/quintin/Post_Process/GPCR/MOR/LIG_path/BU_path/Rep_5-0/ionized.pdb"
+
+sampling_method = "random"
+precision = "SP"
